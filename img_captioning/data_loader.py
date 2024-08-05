2a9d83bc686d63655a06d50f677642fa2cec1f46
komusama <1029673928@qq.com>
2024-08-05 17:42:22 +0800

    可以读取csv标注数据集

--- img_captioning/data_loader.py
+++ img_captioning/data_loader.py
@@ -1,12 +1,9 @@
 import torch
-import torchvision.transforms as transforms
 import torch.utils.data as data
 import os
-import pickle
-import numpy as np
+import pandas as pd
 import nltk
 from PIL import Image
-from build_vocab import Vocabulary
 from pycocotools.coco import COCO
 
 
@@ -52,6 +49,45 @@ class CocoDataset(data.Dataset):
     def __len__(self):
         return len(self.ids)
 
+class CSVDataset(data.Dataset):
+    """CSV Custom Dataset compatible with torch.utils.data.DataLoader."""
+    def __init__(self, root, csv, vocab, transform=None):
+        """Set the path for images, captions and vocabulary wrapper.
+
+        Args:
+            root: image directory.
+            csv: csv file path.
+            vocab: vocabulary wrapper.
+            transform: image transformer.
+        """
+        self.root = root
+        self.csv = csv
+        self.vocab = vocab
+        self.transform = transform
+        self.data = pd.read_csv(csv)
+
+    def __getitem__(self, index):
+        """Returns one data pair (image and caption)."""
+        vocab = self.vocab
+        row = self.data.iloc[index]
+        caption = row['caption']
+        path = row['image']
+
+        image = Image.open(os.path.join(self.root, path)).convert('RGB')
+        if self.transform is not None:
+            image = self.transform(image)
+
+        # Convert caption (string) to word ids.
+        tokens = nltk.tokenize.word_tokenize(str(caption).lower())
+        caption = []
+        caption.append(vocab('<start>'))
+        caption.extend([vocab(token) for token in tokens])
+        caption.append(vocab('<end>'))
+        target = torch.Tensor(caption)
+        return image, target
+
+    def __len__(self):
+        return len(self.data)
 
 def collate_fn(data):
     """Creates mini-batch tensors from the list of tuples (image, caption).
@@ -84,22 +120,36 @@ def collate_fn(data):
         targets[i, :end] = cap[:end]        
     return images, targets, lengths
 
-def get_loader(root, json, vocab, transform, batch_size, shuffle, num_workers):
-    """Returns torch.utils.data.DataLoader for custom coco dataset."""
-    # COCO caption dataset
-    coco = CocoDataset(root=root,
-                       json=json,
-                       vocab=vocab,
-                       transform=transform)
-    
-    # Data loader for COCO dataset
-    # This will return (images, captions, lengths) for each iteration.
-    # images: a tensor of shape (batch_size, 3, 224, 224).
-    # captions: a tensor of shape (batch_size, padded_length).
-    # lengths: a list indicating valid length for each caption. length is (batch_size).
-    data_loader = torch.utils.data.DataLoader(dataset=coco, 
-                                              batch_size=batch_size,
-                                              shuffle=shuffle,
-                                              num_workers=num_workers,
-                                              collate_fn=collate_fn)
+def get_loader(root, json, vocab, transform, batch_size, shuffle, num_workers, type='CoCo'):
+    """Returns torch.utils.data.DataLoader for custom dataset."""
+    if type == 'COCO':
+        # COCO caption dataset
+        coco = CocoDataset(root=root,
+                           json=json,
+                           vocab=vocab,
+                           transform=transform)
+
+        # Data loader for COCO dataset
+        # This will return (images, captions, lengths) for each iteration.
+        # images: a tensor of shape (batch_size, 3, 224, 224).
+        # captions: a tensor of shape (batch_size, padded_length).
+        # lengths: a list indicating valid length for each caption. length is (batch_size).
+        data_loader = torch.utils.data.DataLoader(dataset=coco,
+                                                  batch_size=batch_size,
+                                                  shuffle=shuffle,
+                                                  num_workers=num_workers,
+                                                  collate_fn=collate_fn)
+    elif type == 'CSV':
+        # CSV caption dataset
+        dataset = CSVDataset(root=root,
+                            csv=json,
+                            vocab=vocab,
+                            transform=transform)
+        dataloader = torch.utils.data.DataLoader(dataset=dataset,
+                                                batch_size=batch_size,
+                                                shuffle=shuffle,
+                                                num_workers=num_workers,
+                                                collate_fn=collate_fn)
+    else:
+        raise Exception('Data loader type not supported: {}'.format(type))
     return data_loader
