78c12f9cce71432f24f6bab3673a881952c2929d
komusama <1029673928@qq.com>
2024-08-06 17:12:56 +0800

    gradio api批量标注脚本创建，写不出来

---
+++ caption_generation/monkey.py
@@ -0,0 +1,14 @@
+from gradio_client import Client
+
+client = Client("http://vlrlab-monkey.xyz:7681/")
+result = client.predict(
+		[["你是谁",'']],	# Tuple[str | Dict(file: filepath, alt_text: str | None) | None, str | Dict(file: filepath, alt_text: str | None) | None]  in 'Monkey-Chat' Chatbot component
+		"Hello!",	# str  in 'Input' Textbox component
+		api_name="/add_text"
+)
+print(result)
+result = client.predict(
+		[["Hello!",'']],	# Tuple[str | Dict(file: filepath, alt_text: str | None) | None, str | Dict(file: filepath, alt_text: str | None) | None]  in 'Monkey-Chat' Chatbot component
+		api_name="/predict"
+)
+print(result)
