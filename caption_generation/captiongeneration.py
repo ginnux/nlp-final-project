16e7cc4393f4f5309800db620fc06a91f34fcc2e
komusama <1029673928@qq.com>
2024-08-08 12:54:55 +0800

    利用ali批量创建caption

---
+++ caption_generation/captiongeneration.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from ali import *
+from glob import glob
+from tqdm import tqdm
+
+images = sorted(glob('../images/train/*.jpg'))
+captions = []
+i=0
+
+bar = tqdm(images)
+for image in bar:
+    i+=1
+    if i % 100 == 0:
+        df = pd.DataFrame({'image': images[:i], 'caption': captions})
+        df.to_csv(f'../captions_{i}.csv', index=False)
+    caption = ali_caption(image)
+    captions.append(caption)
+    bar.set_description(f'Captioning {image}')
+
+df = pd.DataFrame({'image': images, 'caption': captions})
+df.to_csv('../captions.csv', index=False)
--- caption_generation/monkey.py
+++ caption_generation/monkey.py
@@ -2,7 +2,7 @@ from gradio_client import Client,file
 
 client = Client("http://vlrlab-monkey.xyz:7681/")
 result = client.predict(
-		[["你是谁",'']],	# Tuple[str | Dict(file: filepath, alt_text: str | None) | None, str | Dict(file: filepath, alt_text: str | None) | None]  in 'Monkey-Chat' Chatbot component
+		[["你是什么llm",'']],	# Tuple[str | Dict(file: filepath, alt_text: str | None) | None, str | Dict(file: filepath, alt_text: str | None) | None]  in 'Monkey-Chat' Chatbot component
 		"Hello!",	# str  in 'Input' Textbox component
 		api_name="/add_text"
 )
@@ -13,8 +13,13 @@ result = client.predict(
 )
 print(result)
 result = client.predict(
-		[["Hello!",'']],	# Tuple[str | Dict(file: filepath, alt_text: str | None) | None, str | Dict(file: filepath, alt_text: str | None) | None]  in 'Monkey-Chat' Chatbot component
+		[["give caption!",'']],	# Tuple[str | Dict(file: filepath, alt_text: str | None) | None, str | Dict(file: filepath, alt_text: str | None) | None]  in 'Monkey-Chat' Chatbot component
 		file("../img_captioning/png/example.png"),
 		api_name="/add_file"
+)
+print(result)
+result = client.predict(
+		[["Hello!",'']],	# Tuple[str | Dict(file: filepath, alt_text: str | None) | None, str | Dict(file: filepath, alt_text: str | None) | None]  in 'Monkey-Chat' Chatbot component
+		api_name="/caption"
 )
 print(result)
