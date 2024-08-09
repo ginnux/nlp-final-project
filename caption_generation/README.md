53a8bb0813c707f07e12c6be6995252473aaf563
ginnux's main computer <coldwarrior@163.com>
2024-08-09 11:22:37 +0800

    建立阿里LLM本地运行程序

--- caption_generation/README.md
+++ caption_generation/README.md
@@ -1,3 +1,25 @@
+# 阿里通义千问 本地Setup使用方法
+1. 配置pytorch环境：
+   - python 3.8及以上版本
+   - pytorch 1.12及以上版本，推荐2.0及以上版本
+   - 建议使用CUDA 11.4及以上
+   - 建议使用RTX3090等24G显存及以上显卡
+2. 安装依赖：
+    ```bash
+    pip install modelscope -U
+    pip install transformers accelerate tiktoken -U
+    pip install einops transformers_stream_generator -U
+    pip install "pillow==9.*" -U
+    pip install torchvision
+    pip install matplotlib -U
+    ```
+3. 下载模型：
+    ```bash
+    modelscope download --model qwen/Qwen-VL-Chat --cache_dir './cache_dir'
+    ```
+4. 配置模型路径，在`ali_local_caption.py`中将`model_dir`设置为`./cache_dir/qwen/Qwen-VL-Chat`（按实际下载的目录更改）。
+5. 运行`ali_local_caption.py`。
+
 # 阿里通义千问 API使用方法
 1. 在[申请网址](https://help.aliyun.com/zh/dashscope/developer-reference/acquisition-and-configuration-of-api-key?spm=a2c4g.11186623.0.0.7b5728c46Soa44)中申请API Key。
 2. 在`ali.py`中填入API Key。
---
+++ caption_generation/ali_local_caption.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from ali import *
+from glob import glob
+from tqdm import tqdm
+
+# ali setup
+from modelscope import (
+    snapshot_download, AutoModelForCausalLM, AutoTokenizer, GenerationConfig
+)
+import torch
+model_id = 'qwen/Qwen-VL-Chat'
+revision = 'v1.1.0'
+
+# 配置到指定的下载好的模型目录
+model_dir = "Qwen-VL-Chat"
+torch.manual_seed(1234)
+
+# 请注意：分词器默认行为已更改为默认关闭特殊token攻击防护。
+tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True)
+# 打开bf16精度，A100、H100、RTX3060、RTX3070等显卡建议启用以节省显存
+# model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="auto", trust_remote_code=True, bf16=True).eval()
+# 打开fp16精度，V100、P100、T4等显卡建议启用以节省显存
+model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="auto", trust_remote_code=True, fp16=True).eval()
+# 使用CPU进行推理，需要约32GB内存
+# model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="cpu", trust_remote_code=True).eval()
+# 默认使用自动模式，根据设备自动选择精度
+# model = AutoModelForCausalLM.from_pretrained(model_dir, device_map="auto", trust_remote_code=True).eval()
+
+# 可指定不同的生成长度、top_p等相关超参
+model.generation_config = GenerationConfig.from_pretrained(model_dir, trust_remote_code=True)
+
+
+
+# ali_caption
+def ali_local_caption(file_path="../img_captioning/png/example.png", prompt="Give a brief caption of the picture."):
+    # 第一轮对话 1st dialogue turn
+    query = tokenizer.from_list_format([
+        {'image': file_path},
+        {'text': prompt},
+    ])
+    response, history = model.chat(tokenizer, query=query, history=None)
+
+    # print(response)
+    # 图中是一名年轻女子在沙滩上和她的狗玩耍，狗的品种是拉布拉多。她们坐在沙滩上，狗的前腿抬起来，与人互动。
+    return response
+
+# 标注代码
+images = sorted(glob('../images/train/*.jpg'))
+captions = []
+i=0
+
+bar = tqdm(images)
+for image in bar:
+    i+=1
+    if i % 100 == 0:
+        df = pd.DataFrame({'image': images[:i], 'caption': captions})
+        df.to_csv(f'../captions_{i}.csv', index=False)
+    caption = ali_local_caption(image)
+    captions.append(caption)
+    bar.set_description(f'Captioning {image}')
+
+df = pd.DataFrame({'image': images, 'caption': captions})
+df.to_csv('../captions.csv', index=False)
